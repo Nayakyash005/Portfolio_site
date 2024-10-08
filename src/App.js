5ec725de252e61bc80eda3e4f5f41ce370e4c51b
Nayakyash005 <yashpankajkumarnayak@gmail.com>
2024-10-08 11:50:48 +0530

    change done

--- src/App.js
+++ src/App.js
@@ -10,30 +10,32 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Tech from "./components/Tech";
 import Footer from "./components/Footer";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const App = () => {
-  return (
-    <>
-      <div className="overflow-x-hidden">
-        <Headers />
-        <ToastContainer />
+  const location = useLocation();
 
-        {/* <img
-        className="fixed top-0 -z-10 h-screen w-full object-cover opacity-60"
-        src="https://lh6.googleusercontent.com/proxy/p0j_F_ba1hPTtS3p0V42nK6hsBye7O9jl78qZyxJBQ7Tr6VKpz9aHHHr6UndLeOGIt2Doe_pNNANSxa6W26MT23UEPAz2JVfOqRMwMYOLvKz=w3840-h2160-p-k-no-nd-mv"
-        alt=""
-      /> */}
-        <Home />
-        {/* <div id="foot-1" className="h-screen w-full bg-red-200"></div> */}
-        {/* <div id="foot-2" className="h-screen w-full bg-red-500"></div> */}
-        {/* <div id="foot-3" className="h-screen w-full bg-blue-200"></div> */}
-        <Skills />
-        <Tech />
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
-    </>
+  useEffect(() => {
+    // Ensure gtag function is available before using it
+    if (typeof window.gtag === "function") {
+      window.gtag("config", "G-688TT3Y0L9", {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
+
+  return (
+    <div className="overflow-x-hidden">
+      <Headers />
+      <ToastContainer />
+      <Home />
+      <Skills />
+      <Tech />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// index.js or main.js
+
+import React from "react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
